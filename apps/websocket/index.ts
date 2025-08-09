91ed4a815021a31e2c321a6c3c1098239ffbbb8f
rohini Garg <rohini2garg1@gmail.com>
2025-08-10 03:03:28 +0530

    Added fe, be, ws

---
+++ apps/websocket/index.ts
@@ -0,0 +1,25 @@
+import { password } from "bun";
+import { prismaClient } from "db/client";
+
+Bun.serve({
+    port: 8081,
+    fetch(req, server) {
+        // Update the request to a websocket
+        if (server.upgrade(req)) {
+           return;  // do not return a Response  
+        }
+        return new Response("Upgrade failed", {status: 500});
+    },
+
+    websocket: {
+        message(ws, message) {
+            prismaClient.user.create({
+                data: {
+                    username: Math.random().toString(),
+                    password: Math.random().toString()
+                }
+            })
+            ws.send(message);
+        },
+    },
+});
